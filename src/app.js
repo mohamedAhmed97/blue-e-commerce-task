e2262b388bd2c0e4e02fb4234227ea8d593ce4cd
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-11-09 15:59:16 +0200

    fix CROS

--- src/app.js
+++ src/app.js
@@ -7,6 +7,13 @@ const brand = require("./routes/brand")
 const category = require("./routes/category")
 const product = require('./routes/product');
 const admin = require('./routes/admin');
+//CROS
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type,Authorization ,Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE,PATCH, OPTIONS');
+    next();
+});
 //json
 app.use(express.json());
 //user route
@@ -19,5 +26,7 @@ app.use("/api", category);
 app.use("/api", product)
 //admin
 app.use("/api", admin)
+
+
 
 module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 const app = require("./app")
-//CROS
-app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, Content-Type,Authorization ,Accept");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE,PATCH, OPTIONS');
-    next();
-});
-
-app.listen(process.env.PORT||3001, () => {
+app.listen(process.env.PORT || 3001, () => {
     console.log("Server Running on port" + process.env.PORT);
 })
